4a8acc93530a782d32c8da796fae4b96063ff01c
Minako Kubo <minako.kubo@gmail.com>
2012-12-20 09:54:20 +0900

    ChromeDriver, SafariDriverSamples

---
+++ src/samplecode/ChromeTest.java
@@ -0,0 +1,89 @@
+package samplecode;
+
+
+import java.io.File;
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class ChromeTest extends TestCase {
+
+  private static ChromeDriverService service;
+  private WebDriver driver;
+
+  @BeforeClass
+  public static void createAndStartService() {
+    service = new ChromeDriverService.Builder()
+        .usingChromeDriverExecutable(new File("/Users/kubo/Dev/Web/selenium/chromedriver"))
+        .usingAnyFreePort()
+        .build();
+    try {
+		service.start();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+  }
+
+  @AfterClass
+  public static void createAndStopService() {
+    service.stop();
+  }
+
+  @Before
+  public void createDriver() {
+//	  DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+//	  capabilities.setCapability("chrome.binary", "/usr/lib/chromium-browser/chromium-browser");
+//    driver = new RemoteWebDriver(service.getUrl(), capabilities);
+	  System.setProperty("webdriver.chrome.driver", "/Users/kubo/Dev/Web/selenium/chromedriver");
+      driver = new ChromeDriver();
+  }
+
+  @After
+  public void quitDriver() {
+    driver.quit();
+  }
+
+  @Test
+  public void testGoogleSearch() {
+    driver.get("http://www.google.com");
+    WebElement searchBox = driver.findElement(By.name("q"));
+    searchBox.sendKeys("webdriver");
+    searchBox.submit();
+    try {
+		Thread.sleep(2000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    assertEquals("webdriver - Google 検索", driver.getTitle());
+    TakesScreenshot sc = (TakesScreenshot) driver;
+    File srcFile = sc.getScreenshotAs(OutputType.FILE);
+    try {
+		FileUtils.copyFile(srcFile, new File("./test003.png"));
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+  }
+}
